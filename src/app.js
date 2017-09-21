d12db7910fefaf6c0a24129ee39c05fbe22544c8
Maurice van Cooten <maurice@minus.nz>
2017-09-22 10:22:02 +1200

    client side JavaAscript

---
+++ src/app.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {BrowserRouter} from 'react-router-dom'
+import Router from './components/router'
+
+ReactDOM.render(
+  <BrowserRouter>
+    <Router/>
+  </BrowserRouter>,
+document.getElementById('content'))
+
+if (process.env.NODE_ENV === 'development') {
+  module.hot.accept()
+}
