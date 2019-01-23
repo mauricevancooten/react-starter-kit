05ea163f87908c1a5f0ab599eafead4fc292ceee
Maurice van Cooten <maurice@kosmo.studio>
2019-01-24 08:25:47 +1300

    CSS Module example

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -2,7 +2,9 @@ import React from 'react'
 import styles from './About.scss'
 
 const About = () => (
-  <h1 className={styles.red}>About</h1>
+  <div>
+    <h1 className={styles.blue}>Blues</h1>
+  </div>
 )
 
 export default About
