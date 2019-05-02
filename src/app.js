b02aa7225e220f44a495bd4e9f2a1ba48c8bd42d
Maurice van Cooten <maurice@kosmo.studio>
2019-05-02 12:41:02 +1200

    Case sensitive

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import {BrowserRouter} from 'react-router-dom'
-import Router from './components/router'
+import Router from './components/Router'
 
 ReactDOM.hydrate(
   <BrowserRouter>
